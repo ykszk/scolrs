a20541b7d937cfbec4dda59f64ec8eb1cd4880c3
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2023-12-07 14:36:09 +0900

    Cleanup python tests

--- crates/pyscol/tests/tests.py
+++ crates/pyscol/tests/tests.py
@@ -1,36 +1,17 @@
-import time
+import unittest
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pydicom
 import pyscol
 from logzero import logger
 
 
-class Timer:
-    def __init__(self, verbose=True):
-        self.start = time.perf_counter()
-        self.verbose = verbose
-
-    def __call__(self):
-        return time.perf_counter() - self.start
-
-    def __enter__(self):
-        self.start = time.perf_counter()
-        return self
-
-    def __exit__(self, exception_type, exception_value, traceback):
-        self.end = time.perf_counter()
-        if self.verbose:
-            print(self.end - self.start)
-
-
-indir = Path("../../../tests/data/dicom")
-for fn in indir.glob("*.dcm"):
-    logger.info("%s", fn)
-    dcm = pydicom.dcmread(fn)
-    arr = dcm.pixel_array
-    logger.info("calc")
-    with Timer() as t:
-        print(pyscol.trimming_param(arr))
+class TestTrim(unittest.TestCase):
+    def test_trim(self):
+        indir = Path("../../../tests/data/dicom")
+        for fn in indir.glob("*.dcm"):
+            logger.info("%s", fn)
+            dcm = pydicom.dcmread(fn)
+            arr = dcm.pixel_array
+            logger.info("calc")
+            print(pyscol.trimming_param(arr))
