bc054552cdebbd97f95d7a129bf3697ab207900c
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2024-11-20 12:22:57 +0900

    Adjust dependency versions to compile

--- crates/devsc/tests/test_trim.rs
+++ crates/devsc/tests/test_trim.rs
@@ -2,9 +2,10 @@ use anyhow::Result;
 use dicom::object::open_file;
 use dicom::pixeldata::image;
 use dicom::pixeldata::image::ImageBuffer;
+use dicom_pixeldata::{ndarray::Array4, ConvertOptions, VoiLutOption};
 use ndarray::Axis;
 
-use devscol::{create_slice, trimming_box_with_resample, Normalize};
+use devscol::{create_slice, trimming_box_with_resample, ImageFilter, PredicateSource};
 use dicom::pixeldata::PixelDecoder;
 use log::{debug, warn};
 use std::{ffi::OsStr, path::PathBuf, sync::Once};
@@ -47,30 +48,31 @@ fn test_dicom_trim() -> Result<()> {
                     pixel_data.number_of_frames()
                 );
             }
+            let options = ConvertOptions::new()
+                .with_voi_lut(VoiLutOption::Normalize)
+                .force_8bit();
+            let u8arr4d: Array4<u8> = pixel_data.to_ndarray_with_options(&options)?;
             let now = std::time::Instant::now();
-            let u8arr3d = match pixel_data.bits_allocated() {
-                8 => pixel_data.to_ndarray_frame::<u8>(0)?,
-                16 => {
-                    let arr = pixel_data.to_ndarray_frame::<i16>(0)?;
-                    arr.minmax_normalize().unwrap() // arr is not empty
-                }
-                i => {
-                    anyhow::bail!("Invalid bits allocated: {}", i)
-                }
-            };
-            if u8arr3d.shape()[2] > 1 {
-                warn!(
-                    "Using first channel of {}-channel image",
-                    u8arr3d.shape()[2]
-                )
-            }
-            let u8arr2d = u8arr3d.index_axis(Axis(2), 0);
+
+            // Remove frame and channel dimensions
+            let u8arr2d = u8arr4d
+                .index_axis_move(Axis(0), 0)
+                .index_axis_move(Axis(2), 0);
 
             let img = u8arr2d.mapv(|e| e as i16);
 
             let resample_step = img.ncols().max(img.nrows()) / 1000 + 1;
             debug!("resample_step: {:?}", resample_step);
-            let bbox = trimming_box_with_resample(img.view(), 0.2, resample_step)?;
+            let bbox = trimming_box_with_resample(
+                img.view(),
+                vec![PredicateSource {
+                    filter: ImageFilter::Original,
+                    quantile_min: Some(0.01),
+                    quantile_max: Some(0.99),
+                    raw: false,
+                }],
+                resample_step,
+            )?;
             let u8arr = u8arr2d
                 .slice(create_slice(&bbox, &(u8arr2d.nrows(), u8arr2d.ncols())))
                 .to_owned();
