d3385eef7de5b60df658d520bf858d6175ad3b09
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2025-01-25 18:39:48 +0900

    Allow stdin for `html` command

--- crates/commands/src/commands/html.rs
+++ crates/commands/src/commands/html.rs
@@ -1,13 +1,19 @@
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{BufWriter, Read, Write};
 
 use crate::cli::HtmlArgs;
 use anyhow::{Context, Result};
 use scolrs::draw::wrap_in_html;
 
 pub fn cmd(args: HtmlArgs) -> Result<()> {
-    let svg = std::fs::read_to_string(args.input.as_path())
-        .with_context(|| format!("Failed to read file: {:?}", args.input))?;
+    let svg = if args.input.as_os_str() == "-" {
+        let mut buffer = String::new();
+        std::io::stdin().read_to_string(&mut buffer)?;
+        buffer
+    } else {
+        std::fs::read_to_string(args.input.as_path())
+            .with_context(|| format!("Failed to read file: {:?}", args.input))?
+    };
     let title = args.title.map_or_else(
         || {
             args.input
