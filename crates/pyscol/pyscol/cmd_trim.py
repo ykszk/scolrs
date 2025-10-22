c727ecdec6b1f8da07ece4345848a79f237e10a5
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2025-10-22 16:31:35 +0900

    Add dicom output to pyscol trim

--- crates/pyscol/pyscol/cmd_trim.py
+++ crates/pyscol/pyscol/cmd_trim.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from tqdm import tqdm
 from datetime import datetime
 from importlib.metadata import version
@@ -11,6 +12,7 @@ import pyscol
 import os
 
 logger.setLevel(os.environ.get('LOGLEVEL', 'INFO').upper())
+
 
 def normalize(x, percentile):
     minmax = np.percentile(x, [percentile, 100 - percentile])
@@ -34,27 +36,28 @@ def add_arguments(parser: argparse.ArgumentParser):
         help='Sets of filter, quantie_min, quantile_max, raw. e.g. "sobel_x,None,0.9,False", "sobel_y,0.1" ,. default: %(default)s',
         type=str,
         nargs='*',
-        default=['original,0.1,0.9,True', 'laplacian,0.1,0.9,True']
+        default=['original,0.1,0.9,True', 'laplacian,0.1,0.9,True'],
     )
-    parser.add_argument(
-        '-j', '--jobs', help='Number of jobs. default: %(default)s', type=int, default=-1
-    )
+    parser.add_argument('-j', '--jobs', help='Number of jobs. default: %(default)s', type=int, default=-1)
 
-
-    parser.add_argument('--ext', help='Input file extension. default: %(default)s', default='.dcm')
+    parser.add_argument('--input_ext', help='Input file extension. default: %(default)s', default='.dcm')
+    parser.add_argument('--output_ext', help='Output file extension. default: %(default)s', default='.jpg')
 
     parser.add_argument(
         '--tags', nargs='*', help="Dicom tags to save in the comment", default=['PixelSpacing', 'ImagerPixelSpacing']
     )
+
 
 class CropParams(BaseModel):
     original_size: list[int]
     window_minmax: list[int]
     crop_tlbr: list[int]
 
+
 class PydicomDataset(BaseModel):
     version: str
     dataset: dict[str, Any]
+
 
 class ExifComment(BaseModel):
     software: str
@@ -64,7 +67,9 @@ class ExifComment(BaseModel):
     misc: Dict[str, Any]
 
 
-def process_file(input: Path, output: Path, tags: list, predicate_sources: list[pyscol.TrimPredicateSource], percentile: float):
+def process_file(
+    input: Path, output: Path, tags: list, predicate_sources: list[pyscol.TrimPredicateSource], percentile: float
+):
     import piexif
     import pydicom
     import pydicom.datadict
@@ -92,22 +97,37 @@ def process_file(input: Path, output: Path, tags: list, predicate_sources: list[
     bbox = pyscol.trimming_param(arr, predicate_sources)
     logger.debug('bbox:%s', bbox)
     trimmed = boundingbox.crop(arr, bbox, margin=0)
-    u8trimemd, minmax = normalize(trimmed, percentile)
-    crop_param = CropParams(original_size=list(arr.shape), window_minmax=minmax.tolist(), crop_tlbr=np.stack(bbox).ravel().tolist())
-    exif_comment.crop_params = crop_param
-    if tags and dcm:
-        ds = pydicom.Dataset()
-        for tag in tags:
-            v = dcm.get(tag)
-            if v is None:
-                continue
-            ds.add(v)
-        exif_comment.misc['pydicom'] = PydicomDataset(version=pydicom.__version__, dataset=ds.to_json_dict())
+    if output.suffix.lower() == '.dcm':
+        if dcm is None:
+            print(f'Input {input} is not a DICOM file. Cannot save as DICOM.')
+            return
 
-    img = Image.fromarray(u8trimemd)
-    uc = helper.UserComment.dump(exif_comment.model_dump_json())
-    exif = piexif.dump({'Exif': {piexif.ExifIFD.UserComment: uc}})
-    img.save(output, exif=exif)
+        dcm.PixelData = trimmed.tobytes()
+        dcm.Rows, dcm.Columns = trimmed.shape
+        dcm.PhotometricInterpretation = 'MONOCHROME2'
+        dcm.SoftwareVersions = f"pyscol {pyscol.__version__}"
+        dcm.ImageComments = json.dumps({'original_size': list(arr.shape), 'crop_tlbr': np.stack(bbox).ravel().tolist()})
+        dcm.compress(pydicom.uid.RLELossless)
+        dcm.save_as(output)
+    else:
+        u8trimemd, minmax = normalize(trimmed, percentile)
+        crop_param = CropParams(
+            original_size=list(arr.shape), window_minmax=minmax.tolist(), crop_tlbr=np.stack(bbox).ravel().tolist()
+        )
+        exif_comment.crop_params = crop_param
+        if tags and dcm:
+            ds = pydicom.Dataset()
+            for tag in tags:
+                v = dcm.get(tag)
+                if v is None:
+                    continue
+                ds.add(v)
+            exif_comment.misc['pydicom'] = PydicomDataset(version=pydicom.__version__, dataset=ds.to_json_dict())
+
+        img = Image.fromarray(u8trimemd)
+        uc = helper.UserComment.dump(exif_comment.model_dump_json())
+        exif = piexif.dump({'Exif': {piexif.ExifIFD.UserComment: uc}})
+        img.save(output, exif=exif)
 
 
 def parse_trim_predicate_source(s: str) -> tuple[str, Optional[float], Optional[float], bool]:
@@ -140,9 +160,12 @@ def main(args: argparse.Namespace):
             print('Output {} is not a directory. Exiting', args.output)
             return 1
         from joblib import Parallel, delayed
+
         Parallel(n_jobs=args.jobs)(
-            delayed(process_file)(input, args.output / input.with_suffix('.jpg').name, tags, pred_sources, args.percentile)
-            for input in tqdm(sorted(args.input.glob('*' + args.ext)))
+            delayed(process_file)(
+                input, args.output / input.with_suffix(args.output_ext).name, tags, pred_sources, args.percentile
+            )
+            for input in tqdm(sorted(args.input.glob('*' + args.input_ext)))
         )
         # for input in tqdm(sorted(args.input.glob('*' + args.ext))):
         #     output = args.output / input.with_suffix('.jpg').name
